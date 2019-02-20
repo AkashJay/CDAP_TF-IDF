e52e41272d379c66501fc28f5b94e8391742eefd
AkashJay <akash.jayaweera@gmail.com>
2019-02-20 07:59:57 +0530

    Change variable names

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,8 +1,7 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
-
 
 import scala.io.Source
 
@@ -17,40 +16,45 @@ object Test {
     val sc = spark.sparkContext
     import spark.implicits._
 
-
     val linesRDD: RDD[String] = sc.textFile("in/Test")
     val splitText: RDD[List[String]] = linesRDD.map(x => x.split("\\s+").map(_.trim).toList)
     val documents: DataFrame = splitText.toDF
+    val docCount: Long = documents.count()
 
     val dfWithDocId = documents.withColumn("doc_id", monotonically_increasing_id()).withColumnRenamed("value", "document")
 
-    val columns = dfWithDocId.columns.map(col) :+
+    val columns: Array[Column] = dfWithDocId.columns.map(col) :+
       (explode(col("document")) as "token")
 
-    val unfoldedDocs = dfWithDocId.select(columns: _*)
+
+    val unfoldedDocs: DataFrame = dfWithDocId.select(columns: _*)
+    unfoldedDocs.show()
 
 
     val tremFrequency = unfoldedDocs.groupBy("doc_id", "token")
       .agg(count("document") as "tf")
+    tremFrequency.printSchema()
 
     val documentFrequency = tremFrequency.groupBy("token")
         .agg(countDistinct("doc_id") as "df")
 
-    documentFrequency.show()
+    documentFrequency.printSchema()
 
 
 
     val calcIdfUdf = udf { df: Long =>
-                math.log(4/df.toDouble +1)
+                math.log(docCount/df.toDouble +1)
     }
 
     val IDF: DataFrame = documentFrequency.withColumn("idf", calcIdfUdf(col("df")))
+   // IDF.printSchema()
 
 
     val TF_IDF = tremFrequency.join(IDF, Seq("token"), "left")
                                .withColumn("tf_idf", col("tf") * col("idf"))
 
-    TF_IDF.show()
+    //TF_IDF.printSchema()
+    //TF_IDF.show()
 
 
 
---
+++ src/main/scala/TestMLlib.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by akash on 2/20/19.
+  */
+object TestMLlib {
+
+}
